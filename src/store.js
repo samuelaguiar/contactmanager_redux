7fe62055e4b358e1b64dea3da732f922cfd4c430
Samuel <samuelaguiarp@gmail.com>
2018-11-10 19:21:59 -0300

    attemp 3 to fix compose error

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initalState = {};
 
@@ -9,7 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initalState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
